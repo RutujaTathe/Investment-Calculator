3cac3b97e68e45e0af4fc66a139fa0c099ed62a2
RutujaTathe <rutujatathe.2211@gmail.com>
2025-03-18 13:51:53 +0530

    updated calculated css file

--- src/app/Components/calculated-investment/calculated-investment.component.ts
+++ src/app/Components/calculated-investment/calculated-investment.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule, NgFor, NgIf } from '@angular/common';
 import { InvestmentServiceService } from 'src/app/Service/investment-service.service';
 
+
 @Component({
   selector: 'app-calculated-investment',
   standalone: true,
@@ -17,4 +18,5 @@ export class CalculatedInvestmentComponent {
     //using signal
     // result=computed(()=>this.investmentService.resultData.asReadOnly();
   }
+  
 }
